c72025f7c6f95a6fd143c5981f12beab80b3764e
sadkalpan <sadkalpan@gmail.com>
2022-12-28 13:32:09 +0300

    construct clean code demo project

---
+++ CleanCodeDemo/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace CleanCodeDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductManager productManager = new ProductManager(new CentralBankServiceAdapter());
+            productManager.Sell(new Product { Id = 1, Name = "Shoe", Price = 1000 },
+                new Customer{ Id=1, Name="Engin Demirog", IsStudent=false, IsOfficer=true }
+                );
+        }
+    }
+     
+    class Customer : IEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsStudent { get; set; }
+        public bool IsOfficer { get; set; }
+    }
+
+    class Product : IEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+    internal interface IEntity
+    {}
+
+    class ProductManager : IProductService
+    {
+        IBankService _bankService;
+
+        public ProductManager(IBankService bankService)
+        {
+            _bankService = bankService;
+        }
+
+        public void Sell(Product product, Customer customer)
+        {
+            decimal price = product.Price;
+            if (customer.IsStudent)
+            {
+                price = price * (decimal)0.90;
+            }
+            else if (customer.IsOfficer)
+            {
+                price = price * (decimal)0.80;
+            }
+           price = _bankService.ConvertRate(new CurrencyRate { Currency=1, Price=1000});
+            Console.WriteLine(price);
+        }
+    }
+    interface IProductService
+    {
+        void Sell(Product product, Customer customer);
+    }
+
+    internal interface IBankService
+    {
+        decimal ConvertRate(CurrencyRate currenyRate);
+    }
+    class CentralBankServiceAdapter : IBankService
+    {
+        CentralBankService _centralBankService = new CentralBankService();
+
+        public decimal ConvertRate(CurrencyRate currenyRate)
+        {
+            return _centralBankService.ConvertRate(currenyRate);
+        }
+    }
+    class CentralBankService
+    {
+        public decimal ConvertRate(CurrencyRate currenyRate)
+        {
+            return currenyRate.Price / (decimal)5.30;
+        }
+    }
+    class FakeBankService : IBankService
+    {
+        public decimal ConvertRate(CurrencyRate currenyRate)
+        {
+            return currenyRate.Price / (decimal)5.25;
+        }
+    }
+
+    class CurrencyRate
+    {
+        public decimal Price { get; set; }
+        public int Currency { get; set; }
+    }
+}
