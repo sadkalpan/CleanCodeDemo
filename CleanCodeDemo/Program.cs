fd65fcce0a9190e8f39b147d463bd740c87eb1b3
sadkalpan <sadkalpan@gmail.com>
2022-12-29 00:34:51 +0300

    Some rebuild project

--- CleanCodeDemo/Program.cs
+++ CleanCodeDemo/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CleanCodeDemo;
+using System;
 
 namespace CleanCodeDemo
 {
@@ -6,20 +7,42 @@ namespace CleanCodeDemo
     {
         static void Main(string[] args)
         {
-            //this is comment
             ProductManager productManager = new ProductManager(new CentralBankServiceAdapter());
             productManager.Sell(new Product { Id = 1, Name = "Shoe", Price = 1000 },
-                new Customer{ Id=1, Name="Engin Demirog", IsStudent=false, IsOfficer=true }
+                new Customer{ Id=1, Name="Engin Demirog" }
                 );
         }
     }
-     
-    class Customer : IEntity
+
+    class Officer : IEntity, IPerson
     {
+        public Officer()
+        {
+            CampaignHandler = new OfficerCampaignHandler();
+        }
         public int Id { get; set; }
         public string Name { get; set; }
-        public bool IsStudent { get; set; }
-        public bool IsOfficer { get; set; }
+        public IPersonCampaignHandler CampaignHandler { get; set; }
+    }
+    class Student : IEntity, IPerson
+    {
+        public Student()
+        {
+            CampaignHandler = new StudentCampaignHandler();
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public IPersonCampaignHandler CampaignHandler { get; set; }
+    }
+    class Customer : IEntity, IPerson
+    {
+        public Customer()
+        {
+            CampaignHandler = new CustomerCampaignHandler();
+        }   
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public IPersonCampaignHandler CampaignHandler { get; set; }
     }
 
     class Product : IEntity
@@ -30,19 +53,22 @@ namespace CleanCodeDemo
     }
     internal interface IEntity
     {}
-
+    internal interface IPerson
+    {
+        int Id { get; set; }
+        string Name { get; set; }
+        IPersonCampaignHandler CampaignHandler { get; set; }
+    }
     class ProductManager : IProductService
     {
-        IBankService _bankService;
-
+        private readonly IBankService _bankService;
         public ProductManager(IBankService bankService)
         {
             _bankService = bankService;
         }
-
-        public void Sell(Product product, Customer customer)
+        public void Sell(Product product, IPerson person)
         {
-            decimal price = product.Price;
+            /*decimal price = product.Price;
             if (customer.IsStudent)
             {
                 price = price * (decimal)0.90;
@@ -50,16 +76,47 @@ namespace CleanCodeDemo
             else if (customer.IsOfficer)
             {
                 price = price * (decimal)0.80;
-            }
-           price = _bankService.ConvertRate(new CurrencyRate { Currency=1, Price=1000});
-            Console.WriteLine(price);
+            }*/
+            var price = person.CampaignHandler.Calculate(product);
+            var exchangePrice = _bankService.ConvertRate(new CurrencyRate
+            {
+                Currency = 1,
+                Price = price
+            });
+            Console.WriteLine(person.Name + " İsimli Müşterimiz için indirim Karşılığı:" + price.ToString("#.##"));
+            Console.WriteLine("Ürünün Döviz Karşılığı:" + exchangePrice.ToString("#.##"));
+            Console.WriteLine("#####################################################");
         }
     }
     interface IProductService
     {
-        void Sell(Product product, Customer customer);
+        void Sell(Product product, IPerson person);
     }
-
+    internal interface IPersonCampaignHandler
+    {
+        decimal Calculate(Product product);
+    }
+    class StudentCampaignHandler : IPersonCampaignHandler
+    {
+        public decimal Calculate(Product product)
+        {
+            return product.Price * (decimal)0.90;
+        }
+    }
+    class CustomerCampaignHandler : IPersonCampaignHandler
+    {
+        public decimal Calculate(Product product)
+        {
+            return product.Price;
+        }
+    }
+    class OfficerCampaignHandler : IPersonCampaignHandler
+    {
+        public decimal Calculate(Product product)
+        {
+            return product.Price * (decimal)0.80;
+        }
+    }
     internal interface IBankService
     {
         decimal ConvertRate(CurrencyRate currenyRate);
